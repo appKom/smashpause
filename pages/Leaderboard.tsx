44586cddc23c3d7a75dfb973558169d78a9e12c5
Matias Opsahl <matiasop@stud.ntnu.no>
2022-02-13 15:01:35 +0100

    designa leaderboard

--- pages/Leaderboard.tsx
+++ pages/Leaderboard.tsx
@@ -1,6 +1,17 @@
+import { Heading } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import { Key, useEffect, useState } from 'react'
 import styles from '../styles/Leaderboard.module.css'
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+  } from '@chakra-ui/react'
 
 const Leaderboard: NextPage = () => {
     
@@ -29,18 +40,42 @@ const Leaderboard: NextPage = () => {
 
     return (
         <div className={styles.container}>
-            <h1>Leaderboard</h1>
-            <ul>
-                {
-                    users
-                    .sort((a: { rank: number; }, b: { rank: number; }) => {
-                        return b.rank - a.rank
-                    })
-                    .map((user: { id: Key | null | undefined; owID: string; rank: string; }) => {
-                    return <li key={ user.id }> { user.owID + ": " + user.rank } </li>
-                    })
-                }
-            </ul>
+            <Table
+                marginTop={"15%"}
+                width={"23%"} 
+                variant='simple'
+                textAlign={"center"}
+                boxShadow={"1px 1px 10px 10px lightblue"}
+                padding={"1%"}    
+                colorScheme={"teal"}            
+            >
+                <Thead 
+                    border={"1px solid black"}
+                >
+                    <Tr
+                        color={"#2066D7"}
+                        fontSize={"30"}
+                    >
+                        <Th>Name</Th>
+                        <Th>Points</Th>
+                    </Tr>
+                </Thead>
+                <Tbody
+                    color={"#515458"}
+                >
+                    {
+                        users
+                        .sort((a: { rank: number; }, b: { rank: number; }) => {
+                            return b.rank - a.rank
+                        })
+                        .map((user: { id: Key | null | undefined; owID: string; rank: string; }) => {
+                        return <Tr key={ user.id }>
+                                    <Td>{user.owID}</Td><Td>{user.rank}</Td>
+                                </Tr>
+                        })
+                    }
+                </Tbody>
+            </Table>
         </div>
     )
 }
