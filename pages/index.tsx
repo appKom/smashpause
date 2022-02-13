e111ac2ef0966bcee756650dbf36d4991381e2fc
Andreas Haugli <andreashaugli@dhcp-10-22-14-254.wlan.ntnu.no>
2022-02-13 14:52:26 +0100

    Recent matches displayed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,50 @@
 import type { NextPage } from 'next'
-import { Key, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
 
   // Trenger et interface på denne
-  const [users, setUsers] = useState<any>([])
+  const [matches, setMatches] = useState<any>([])
 
   /*
     Fetcher alle brukere til JSON format,
     setter brukere til users-variabelen
   */
   useEffect(() => {
-    const getUsers = async () => {
+    const getMatches = async () => {
       try {
-        const url = "http://localhost:3000/api/user";
+        const url = "http://localhost:3000/api/match";
         const response = await fetch(url, {
           method: "GET"
         });
         const data = await response.json();
-        setUsers(data.users)
+        setMatches(data.matches)
       } catch (err) {
         console.log(err)
       }
     }
-    (async () => await getUsers())();
+    (async () => await getMatches())();
   }, [])
 
   return (
     <div className={styles.container}>
-      <h1>Leaderboard</h1>
+      <h1>Recent matches</h1>
       { //Shitty løsning, men funke inntil videre
-        users
-          .sort((a: { rank: number; }, b: { rank: number; }) => {
-            return b.rank - a.rank
-          })
-          .map((user: { id: Key | null | undefined; owID: string; rank: string; }) => {
-          return <div key={ user.id }> { user.owID + ": " + user.rank } </div>
+        matches.map((match: { updatedAt: string; playerIDs: Array<String>; winnerID: string; }) => {
+          let date = new Date(match.updatedAt);
+
+          return <div className={styles.matchInfo}>
+                    <div className={styles.matchDate}>
+                      {date.toLocaleDateString()}
+                    </div>
+                    <div className={styles.playerInfo}>
+                      {match.playerIDs[0] + " vs. " + match.playerIDs[1]}
+                    </div>
+                    <div className={styles.matchWinner}>
+                      Winner: {match.winnerID}
+                    </div> 
+                  </div>
         })
       }
     </div>
@@ -44,3 +52,13 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+/**
+ * matches
+          .sort((a: { rank: number; }, b: { rank: number; }) => {
+            return b.rank - a.rank
+          })
+          .map((user: { id: Key | null | undefined; owID: string; rank: string; }) => {
+          return <div key={ user.id }> { user.owID + ": " + user.rank } </div>
+    })
+ */
