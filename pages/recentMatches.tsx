e117dddd6a55fd677ef32a5b5253e517904eb048
Robin Tveråen <robintve@stud.ntnu.no>
2022-03-27 14:13:20 +0200

    added icons for recentmatches

--- pages/recentMatches.tsx
+++ pages/recentMatches.tsx
@@ -5,6 +5,7 @@ import {
   GridItem,
   Heading,
   Spacer,
+  Image,
 } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
@@ -13,7 +14,10 @@ import styles from "../styles/recentMatches.module.css";
 const recentMatches: NextPage = () => {
   // Trenger et interface på denne
   const [matches, setMatches] = useState<any>([]);
+  const [string, setString] = useState<String>();
   
+  
+
   /*
   Fetcher alle brukere til JSON format,
   setter brukere til users-variabelen
@@ -22,6 +26,7 @@ const recentMatches: NextPage = () => {
   useEffect(() => {
     const getMatches = async () => {
       try {
+        setString("characters/1.png");
         const url = "http://localhost:3000/api/match";
         const response = await fetch(url, {
           method: "GET",
@@ -35,66 +40,104 @@ const recentMatches: NextPage = () => {
     (async () => await getMatches())();
   }, []);
 
-
   return (
-    <Box
-    marginTop={"%"}
-    >
-    <Heading mx="auto" marginLeft={"40%"}>Recent matches</Heading>
+    <Box>
+      <Heading mx="auto" marginLeft={"40%"}>
+        Recent matches
+      </Heading>
 
-    <Flex margintop="20%">
-      <Grid
-        bgColor={"lightblue"}
-        gap={6}
-        templateRows="repeat(5, 1fr)"
-        mx="auto"
+      <Flex
+        bgColor={"#13a8d1"}
         width="50%"
-        boxShadow={"2px 2px 5px black"}
+        margin={"auto"}
+        padding={"1%"}
+        borderRadius={"50px"}
       >
-      
-        {
-          //Shitty løsning, men funke inntil videre
-          matches.map(
-            (match: {
-              updatedAt: string;
-              playerIDs: Array<String>;
-              winnerID: string;
-            }) => {
-              let date = new Date(match.updatedAt);
+        <Grid gap={6} mx="auto" width="80%" fontSize="xl">
+          {
+            //Shitty løsning, men funke inntil videre
+            matches.map(
+              (match: {
+                updatedAt: string;
+                playerIDs: Array<String>;
+                winnerID: string;
+              }) => {
+                let date = new Date(match.updatedAt);
+                // Trenger egen variabel i database som lagrer karakteren til brukerne med f.eks "1.png"
+                return (
+                  <Box>
+                    <Flex
+                    margin={"auto"}
+                    width="70%"
+                    >
+                    <Image alt={""}
+                    src={match.winnerID}
+                    bgColor={"green"}
+                    width={50}>
+                    </Image>
+                    <Spacer></Spacer>
+                    <Image alt={""}
+                    src={match.winnerID}
+                    bgColor={"green"}
+                    width={50}>
+                    </Image>
+                    <Spacer></Spacer>
+                    <Image alt={""}
+                    src={match.winnerID}
+                    bgColor={"green"}
+                    width={50}>
+                    </Image>
+                    <Spacer></Spacer>
+                    <Image alt={""}
+                    src={match.winnerID}
+                    bgColor={"green"}
+                    width={50}>
+                    </Image>
+                    <Spacer></Spacer>
 
-              return (
-                <GridItem
-                  bgColor={"white"}
-                  display={"inline"}
-                  mx="auto"
-                  width="70%"
-                  marginTop="2%"
-                  borderRadius={6}
-                >
-                  <Flex
-                    padding={5}
-                  >
-                    <Box>{date.toLocaleDateString()}</Box>
-                      <Spacer />
-                    <Box>{match.playerIDs[0]}</Box>
-                      <Spacer />
-                    <Box>VS</Box>
-                      <Spacer />
-                    <Box>{match.playerIDs[1]}</Box>
-                      <Spacer />
-                    <Box width={150}>
-                      Winner: {match.winnerID}
-                    </Box>
-                  </Flex>
-                </GridItem>
-              );
-            }
-          )
+                    <Image alt={""}
+                    src={match.winnerID}
+                    bgColor={"green"}
+                    width={50}>
+                    </Image>
+                    <Spacer></Spacer>
 
-        }
-      </Grid>
-    </Flex>
-  </Box>
+                    <Image alt={""}
+                    src={match.winnerID}
+                    bgColor={"green"}
+                    width={50}>
+                    </Image>
+                    
+                    
+                    </Flex>
+                    <GridItem
+                      bgColor={"white"}
+                      mx="auto"
+                      width="70%"
+                      borderRadius={6}
+                      boxShadow={"2px 2px 5px black"}
+                      height={100}
+                    >
+                      <Flex padding={5}>
+                        <Box>{date.toLocaleDateString()}</Box>
+
+                        <Box>{match.playerIDs[0]}</Box>
+
+                        <Box>VS</Box>
+
+                        <Box>{match.playerIDs[1]}</Box>
+
+                        <Box width={150}>Winner: {match.winnerID}</Box>
+                      </Flex>
+                    </GridItem>
+                  </Box>
+                );
+              }
+            )
+          }
+        </Grid>
+      </Flex>
+    </Box>
   );
 };
 
